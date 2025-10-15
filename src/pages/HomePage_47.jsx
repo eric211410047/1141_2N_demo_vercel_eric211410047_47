03300ec3a769afc18a59181de7cc7967879cf75c
eric211410047 <ericericeric25782578@gmail.com>
2025-10-15 20:39:43 +0800

    add tailwind support1

--- src/pages/HomePage_47.jsx
+++ src/pages/HomePage_47.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage_47 = () => {
   return (
     <div className='flex flex-col items-center justify-center '>
-      <h1 className='text-2xl text-indigo-700'>HomePage_47</h1>
+      <h1 className='text-2xl text-purple-700'>HomePage_47</h1>
       <h2 className='text-xl'>eric,211410047</h2>
     </div>
   );
